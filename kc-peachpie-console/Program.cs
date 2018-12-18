56e86bd7b71d2a8f88fca8b82a06ec3000be42a1
PetrŠvihlík <svihlik.petr@gmail.com>
2018-12-18 18:45:24 +0100

    null params

--- kc-peachpie-console/Program.cs
+++ kc-peachpie-console/Program.cs
@@ -11,8 +11,9 @@ namespace kc_peachpie_console
         {
             using (var ctx = Context.CreateConsole(string.Empty, args))
             {
+                ctx.Include(null,"autoload.php");
                 TestInterop1(ctx);
-                TestInterop2(ctx);
+                //TestInterop2(ctx);
             }
             Console.ReadLine();
         }
@@ -26,7 +27,8 @@ namespace kc_peachpie_console
 
         public static void TestInterop2(Context ctx)
         {
-            DeliveryClient deliveryClient = new DeliveryClient(ctx, "975bf280-fd91-488c-994c-2f04416e5ee3");
+            // var urlBuilder = new UrlBuilder(ctx, "975bf280-fd91-488c-994c-2f04416e5ee3", false); // works
+            DeliveryClient deliveryClient = new DeliveryClient(ctx, "975bf280-fd91-488c-994c-2f04416e5ee3", null, null, false, false, 0);
             PhpValue result = deliveryClient.getItems();
             foreach (var item in result)
             {
